2444eea93ad3672e0f705fdfbef7bcecf7af138c
Jacky Su <jacky19918@gmail.com>
2024-03-15 01:08:11 +0800

    update context

--- src/apis/openAIApi.ts
+++ src/apis/openAIApi.ts
@@ -12,3 +12,89 @@ export const callOpenAIApi = async (
   const completion = await openai.chat.completions.create(params);
   return completion.choices[0];
 };
+
+export const callIsUIChangedApi = async (
+  question: string,
+  callback: (result: OpenAI.Chat.Completions.ChatCompletion.Choice) => void
+) => {
+  try {
+    const result = await callOpenAIApi({
+      messages: [
+        {
+          role: "system",
+          content:
+            "你是一個E2E test的專家, 你擅長分析用自然語言描述的每一個步驟的執行後, 畫面是否有變化, 有變化的標示Y, 沒變化的標示N",
+        },
+        {
+          role: "user",
+          content: `
+            1. 前往https://www.youtube.com/
+            2. 在上方的搜尋框輸入"iphone"
+            3. 按ENTER鍵
+            4. 點選右上角"Filters"按鈕
+            5. 點選"Last hour"
+            6. 預期不應該出現"hours ago"字樣
+          `,
+        },
+        {
+          role: "assistant",
+          content: "1. Y\n2. Y\n3. Y\n4. Y\n5. Y\n6. N",
+        },
+        { role: "user", content: question },
+      ],
+      model: "gpt-4-turbo-preview",
+    });
+    callback(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const callOneStepPlayWrightApi = async (
+  text: string,
+  callback: (result: OpenAI.Chat.Completions.ChatCompletion.Choice) => void
+) => {
+  try {
+    const result = await callOpenAIApi({
+      messages: [
+        {
+          role: "system",
+          content:
+            "你是一個E2E test的專家, 你擅長根據一句自然語言, 產生對應的playwright code",
+        },
+        {
+          role: "user",
+          content:
+            "1. 前往https://goodinfo.tw/tw/StockDetail.asp?STOCK_ID=2330",
+        },
+        {
+          role: "assistant",
+          content: 'await page.goto("https://irs.thsrc.com.tw/IMINT/");',
+        },
+        {
+          role: "user",
+          content: '2. 點選"除權息日程"',
+        },
+        {
+          role: "assistant",
+          content: "await page.click('text=\"除權息日程\"');",
+        },
+        {
+          role: "user",
+          content:
+            '3. 預期page的title要出現"(2330) 台積電 除權除息日程一覽表 - Goodinfo!台灣股市資訊網"',
+        },
+        {
+          role: "assistant",
+          content:
+            'await expect(page).toHaveTitle("(2330) 台積電 除權除息日程一覽表 - Goodinfo!台灣股市資訊網");',
+        },
+        { role: "user", content: text },
+      ],
+      model: "gpt-4-turbo-preview",
+    });
+    callback(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { callOpenAIApi } from "@/apis/openAIApi";
-import { TextareaAutosize } from "@mui/base/TextareaAutosize";
+import { callIsUIChangedApi, callOneStepPlayWrightApi } from "@/apis/openAIApi";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -10,10 +9,10 @@ import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { blue, grey } from "@mui/material/colors";
 import { styled } from "@mui/system";
 import OpenAI from "openai";
 import { useMemo, useState } from "react";
+import StyledTextareaAutosize from "./components/StyledTextareaAutosize";
 
 const StyledTypography = styled(Typography)(
   `
@@ -21,162 +20,52 @@ const StyledTypography = styled(Typography)(
   `
 );
 
-const StyledTextareaAutosize = styled(TextareaAutosize)(
-  ({ theme }) => `
-  box-sizing: border-box;
-  width: 100%;
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-size: 0.875rem;
-  font-weight: 400;
-  line-height: 1.5;
-  padding: 8px 12px;
-  border-radius: 8px;
-  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
-  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 2px ${
-    theme.palette.mode === "dark" ? grey[900] : grey[50]
-  };
-
-  &:hover {
-    border-color: ${blue[400]};
-  }
-
-  &:focus {
-    border-color: ${blue[400]};
-    box-shadow: 0 0 0 3px ${
-      theme.palette.mode === "dark" ? blue[600] : blue[200]
-    };
-  }
-
-  // firefox
-  &:focus-visible {
-    outline: 0;
-  }
-`
-);
-
 export default function Home() {
   const [stepsScript, setStepsScript] = useState(`
-  1. 前往https://translate.google.com/?sl=en&tl=zh-TW&text=Jacky&op=translate
-  2. 預期要出現'傑基'
+  1. 前往https://goodinfo.tw/tw/StockDetail.asp?STOCK_ID=2330
+  2. 點選"除權息日程"
+  3. 預期page的title要出現"(2330) 台積電 除權除息日程一覽表 - Goodinfo!台灣股市資訊網"
   `);
-  const [stepsCategory, setStepsCategory] =
+  const [isUIChangedQueryResult, setIsUIChangedQueryResult] =
     useState<OpenAI.ChatCompletion.Choice | null>(null);
   const [currentStep, setCurrentStep] = useState(0);
-  const [oneLineCodes, setOneLineCodes] = useState<
-    OpenAI.ChatCompletion.Choice[]
-  >([]);
-  const callStepCategoryApi = async (question: string) => {
-    try {
-      const result = await callOpenAIApi({
-        messages: [
-          {
-            role: "system",
-            content:
-              "你是一個E2E test的專家, 你擅長分析一段自然語言的每一個步驟屬於什麼category",
-          },
-          {
-            role: "user",
-            content: `
-              1. 前往https://goodinfo.tw/tw/StockDetail.asp?STOCK_ID=2330
-              2. 點選"除權息日程"
-              3. 預期page的title要出現"(2330) 台積電 除權除息日程一覽表 - Goodinfo!台灣股市資訊網"
-            `,
-          },
-          {
-            role: "assistant",
-            content: "1. goto url\n2. click\n3. expect page title",
-          },
-          { role: "user", content: question },
-        ],
-        model: "gpt-4-turbo-preview",
-      });
-      setStepsCategory(result);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const [allStepPlayWrightQueryResult, setAllStepPlayWrightQueryResult] =
+    useState<OpenAI.ChatCompletion.Choice[]>([]);
 
-  const callGetPlayWrightApi = async (obj: {
-    text: string;
-    category: string;
-  }) => {
-    try {
-      const result = await callOpenAIApi({
-        messages: [
-          {
-            role: "system",
-            content:
-              "你是一個E2E test的專家, 你擅長根據一句自然語言和一個category, 產生對應的playwright code",
-          },
-          {
-            role: "user",
-            content: JSON.stringify({
-              text: "1. 前往https://goodinfo.tw/tw/StockDetail.asp?STOCK_ID=2330",
-              category: "goto url",
-            }),
-          },
-          {
-            role: "assistant",
-            content: 'await page.goto("https://irs.thsrc.com.tw/IMINT/");',
-          },
-          {
-            role: "user",
-            content: JSON.stringify({
-              text: '2. 點選"除權息日程"',
-              category: "click",
-            }),
-          },
-          {
-            role: "assistant",
-            content: "await page.click('text=\"除權息日程\"');",
-          },
-          {
-            role: "user",
-            content: JSON.stringify({
-              text: '3. 預期page的title要出現"(2330) 台積電 除權除息日程一覽表 - Goodinfo!台灣股市資訊網"',
-              category: "expect page title",
-            }),
-          },
-          {
-            role: "assistant",
-            content:
-              'await expect(page).toHaveTitle("(2330) 台積電 除權除息日程一覽表 - Goodinfo!台灣股市資訊網");',
-          },
-          { role: "user", content: JSON.stringify(obj) },
-        ],
-        model: "gpt-4-turbo-preview",
-      });
-      setOneLineCodes((prev) => [...prev, result]);
-    } catch (e) {
-      console.log(e);
-    }
-  };
   const handleTextAreChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setStepsScript(e.target.value);
   };
   const handleGetStepsCategoryButtonClick = () => {
-    callStepCategoryApi(stepsScript);
+    callIsUIChangedApi(stepsScript, (result) =>
+      setIsUIChangedQueryResult(result)
+    );
   };
   const currentStepText = useMemo(
     () => stepsScript.trim().split("\n")[currentStep],
     [stepsScript, currentStep]
   );
-  const currentStepCategory = useMemo(
-    () => stepsCategory?.message.content?.trim().split("\n")[currentStep] || "",
-    [stepsCategory, currentStep]
-  );
+  const isUIChangedAfterCurrentStep = useMemo(() => {
+    const nextStep = currentStep + 1;
+    const isUIChangedAfterSteps = isUIChangedQueryResult?.message.content
+      ?.trim()
+      .split("\n");
+    if (isUIChangedAfterSteps && isUIChangedAfterSteps.length > nextStep) {
+      return isUIChangedAfterSteps[nextStep] === "Y";
+    }
+    return false;
+  }, [isUIChangedQueryResult, currentStep]);
   const currentStepObject = useMemo(
     () => ({
       text: currentStepText,
-      category: currentStepCategory,
+      isUIChanged: isUIChangedAfterCurrentStep,
     }),
-    [currentStepCategory, currentStepText]
+    [isUIChangedAfterCurrentStep, currentStepText]
   );
 
   const handleGetOneLineCodeButtonClick = () => {
-    callGetPlayWrightApi(currentStepObject);
+    callOneStepPlayWrightApi(currentStepText, (result) =>
+      setAllStepPlayWrightQueryResult((prev) => [...prev, result])
+    );
     setCurrentStep((prev) => prev + 1);
   };
   return (
@@ -209,7 +98,7 @@ export default function Home() {
             <Card>
               <CardContent>
                 <StyledTypography variant="body2" color="text.secondary">
-                  {JSON.stringify(stepsCategory, null, 4)}
+                  {JSON.stringify(isUIChangedQueryResult, null, 4)}
                 </StyledTypography>
               </CardContent>
             </Card>
@@ -219,14 +108,7 @@ export default function Home() {
             <Card>
               <CardContent>
                 <StyledTypography variant="body2" color="text.secondary">
-                  {JSON.stringify(
-                    {
-                      text: currentStepText,
-                      category: currentStepCategory,
-                    },
-                    null,
-                    4
-                  )}
+                  {currentStepText}
                 </StyledTypography>
               </CardContent>
             </Card>
@@ -239,7 +121,7 @@ export default function Home() {
             <Card>
               <CardContent>
                 <StyledTypography variant="body2" color="text.secondary">
-                  {JSON.stringify(oneLineCodes, null, 4)}
+                  {JSON.stringify(allStepPlayWrightQueryResult, null, 4)}
                 </StyledTypography>
               </CardContent>
             </Card>
