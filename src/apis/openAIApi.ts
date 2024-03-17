af5cff419a905fd35bba3fd4751a662724ff67c9
Jacky Su <jacky19918@gmail.com>
2024-03-17 21:31:45 +0800

    feat: add snapshot example

--- src/apis/openAIApi.ts
+++ src/apis/openAIApi.ts
@@ -1,3 +1,4 @@
+import { home, press } from "@/snapshot/temp";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
@@ -30,7 +31,7 @@ export const callIsUIChangedApi = async (
           content: `
             1. 前往https://www.youtube.com/
             2. 在上方的搜尋框輸入"iphone"
-            3. 按ENTER鍵
+            3. 在上方的搜尋框按ENTER鍵
             4. 點選右上角"Filters"按鈕
             5. 點選"Last hour"
             6. 預期不應該出現"hours ago"字樣
@@ -50,7 +51,7 @@ export const callIsUIChangedApi = async (
   }
 };
 
-export const callOneStepPlayWrightApi = async (
+export const callGetCodeWithTextApi = async (
   text: string,
   callback: (result: OpenAI.Chat.Completions.ChatCompletion.Choice) => void
 ) => {
@@ -60,7 +61,7 @@ export const callOneStepPlayWrightApi = async (
         {
           role: "system",
           content:
-            "你是一個E2E test的專家, 你擅長根據一句自然語言, 產生對應的playwright code",
+            "你是一個E2E test的專家, 你擅長根據一句自然語言, 產生對應的playwright code, 妳的回應只會有code, 不會包含任何markdown的code block'```'或其他多餘的文字",
         },
         {
           role: "user",
@@ -90,6 +91,55 @@ export const callOneStepPlayWrightApi = async (
             'await expect(page).toHaveTitle("(2330) 台積電 除權除息日程一覽表 - Goodinfo!台灣股市資訊網");',
         },
         { role: "user", content: text },
+      ],
+      model: "gpt-4-turbo-preview",
+    });
+    callback(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const callGetCodeWithTextAndSnapshotApi = async (
+  obj: {
+    text: string;
+    snapshot: string;
+  },
+  callback: (result: OpenAI.Chat.Completions.ChatCompletion.Choice) => void
+) => {
+  try {
+    const result = await callOpenAIApi({
+      messages: [
+        {
+          role: "system",
+          content:
+            "你是一個E2E test的專家, 你擅長根據一句自然語言和一個目前畫面的html快照找到對應的element並執行動作, 產生合適的playwright code, 格式會是json: {text: string, snapshot: string}",
+        },
+        {
+          role: "user",
+          content: JSON.stringify({
+            text: '2. 在上方的搜尋框輸入"iphone"',
+            snapshot: home,
+          }),
+        },
+        {
+          role: "assistant",
+          content:
+            "await page.fill('xpath=/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input', 'iphone');",
+        },
+        {
+          role: "user",
+          content: JSON.stringify({
+            text: '4. 點選右上角"Filters"按鈕',
+            snapshot: press,
+          }),
+        },
+        {
+          role: "assistant",
+          content:
+            "await page.click('xpath=/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/div/ytd-search-header-renderer/div[3]/ytd-button-renderer/yt-button-shape/button');",
+        },
+        { role: "user", content: JSON.stringify(obj) },
       ],
       model: "gpt-4-turbo-preview",
     });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { callIsUIChangedApi, callOneStepPlayWrightApi } from "@/apis/openAIApi";
+import {
+  callGetCodeWithTextAndSnapshotApi,
+  callGetCodeWithTextApi,
+  callIsUIChangedApi,
+} from "@/apis/openAIApi";
+import { fill } from "@/snapshot/temp";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -20,6 +25,14 @@ const StyledTypography = styled(Typography)(
   `
 );
 
+const StyledBox = styled(Box)(
+  `
+  width: 100%;
+  max-height: 300px;
+  overflow: auto;
+  `
+);
+
 export default function Home() {
   const [stepsScript, setStepsScript] = useState(`
   1. 前往https://goodinfo.tw/tw/StockDetail.asp?STOCK_ID=2330
@@ -29,8 +42,14 @@ export default function Home() {
   const [isUIChangedQueryResult, setIsUIChangedQueryResult] =
     useState<OpenAI.ChatCompletion.Choice | null>(null);
   const [currentStep, setCurrentStep] = useState(0);
+  const [previousSnapshot, setPreviousSnapshot] = useState<string>("1");
   const [allStepPlayWrightQueryResult, setAllStepPlayWrightQueryResult] =
     useState<OpenAI.ChatCompletion.Choice[]>([]);
+  const [allCode, setAllCode] = useState(`
+const browser = await chromium.launch({ headless: false });
+const context = await browser.newContext();
+const page = await context.newPage();
+  `);
 
   const handleTextAreChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setStepsScript(e.target.value);
@@ -62,9 +81,20 @@ export default function Home() {
     [isUIChangedAfterCurrentStep, currentStepText]
   );
 
-  const handleGetOneLineCodeButtonClick = () => {
-    callOneStepPlayWrightApi(currentStepText, (result) =>
-      setAllStepPlayWrightQueryResult((prev) => [...prev, result])
+  const handleGetCodeWithTextButtonClick = async () => {
+    callGetCodeWithTextApi(currentStepText, (result) => {
+      setAllStepPlayWrightQueryResult((prev) => [...prev, result]);
+      setAllCode((prev) => `${prev}\n${result.message.content}`);
+    });
+    setCurrentStep((prev) => prev + 1);
+  };
+  const handleGetCodeWithTextAndSnapshotButtonClick = () => {
+    callGetCodeWithTextAndSnapshotApi(
+      {
+        text: "3. 在上方的搜尋框按ENTER鍵",
+        snapshot: fill,
+      },
+      (result) => setAllStepPlayWrightQueryResult((prev) => [...prev, result])
     );
     setCurrentStep((prev) => prev + 1);
   };
@@ -74,57 +104,86 @@ export default function Home() {
         <CssBaseline />
         <Container maxWidth="lg">
           <Box sx={{ height: "100vh" }}>
-            <Stack
-              direction="row"
-              spacing={2}
-              justifyContent="space-between"
-              alignItems="center"
-            >
+            <Stack spacing={1}>
               <Typography variant="h6" gutterBottom>
                 Test scenario
               </Typography>
+              <StyledTextareaAutosize
+                minRows={10}
+                value={stepsScript}
+                onChange={handleTextAreChange}
+              />
+              <Button onClick={handleGetStepsCategoryButtonClick}>
+                Get the UI will change after the step
+              </Button>
+              <Typography variant="h6" gutterBottom>
+                Query result: The UI will change after the step
+              </Typography>
+              <Card>
+                <CardContent>
+                  <StyledTypography variant="body2" color="text.secondary">
+                    {JSON.stringify(isUIChangedQueryResult, null, 4)}
+                  </StyledTypography>
+                </CardContent>
+              </Card>
+
+              <Stack direction="row" alignItems="center">
+                <StyledBox>
+                  <Typography variant="h6" gutterBottom>
+                    Current Step
+                  </Typography>
+                  <Card>
+                    <CardContent>
+                      <StyledTypography variant="body2" color="text.secondary">
+                        {currentStepText}
+                      </StyledTypography>
+                    </CardContent>
+                  </Card>
+                </StyledBox>
+                <StyledBox>
+                  <Typography variant="h6" gutterBottom>
+                    Previous Snapshot
+                  </Typography>
+                  <Card>
+                    <CardContent>
+                      <StyledTypography variant="body2" color="text.secondary">
+                        {previousSnapshot}
+                      </StyledTypography>
+                    </CardContent>
+                  </Card>
+                </StyledBox>
+              </Stack>
+
+              <Button onClick={handleGetCodeWithTextButtonClick}>
+                Get code with text
+              </Button>
+              <Button onClick={handleGetCodeWithTextAndSnapshotButtonClick}>
+                Get code with text & snapshot
+              </Button>
+              <Stack direction="row" alignItems="center">
+                <StyledBox>
+                  <Typography variant="h6" gutterBottom>
+                    Each steps query result
+                  </Typography>
+                  <Card>
+                    <CardContent>
+                      <StyledTypography variant="body2" color="text.secondary">
+                        {JSON.stringify(allStepPlayWrightQueryResult, null, 4)}
+                      </StyledTypography>
+                    </CardContent>
+                  </Card>
+                </StyledBox>
+                <StyledBox>
+                  <Card>
+                    <CardContent>
+                      <StyledTypography variant="body2" color="text.secondary">
+                        {allCode}
+                      </StyledTypography>
+                    </CardContent>
+                  </Card>
+                </StyledBox>
+              </Stack>
             </Stack>
-            <StyledTextareaAutosize
-              minRows={10}
-              value={stepsScript}
-              onChange={handleTextAreChange}
-            />
-            <Button onClick={handleGetStepsCategoryButtonClick}>
-              GetStepsCategory
-            </Button>
-            <Typography variant="h6" gutterBottom>
-              StepsCategory
-            </Typography>
-            <Card>
-              <CardContent>
-                <StyledTypography variant="body2" color="text.secondary">
-                  {JSON.stringify(isUIChangedQueryResult, null, 4)}
-                </StyledTypography>
-              </CardContent>
-            </Card>
-            <Typography variant="h6" gutterBottom>
-              Current Step
-            </Typography>
-            <Card>
-              <CardContent>
-                <StyledTypography variant="body2" color="text.secondary">
-                  {currentStepText}
-                </StyledTypography>
-              </CardContent>
-            </Card>
-            <Button onClick={handleGetOneLineCodeButtonClick}>
-              GetOneLineCode
-            </Button>
-            <Typography variant="h6" gutterBottom>
-              OneLineCode
-            </Typography>
-            <Card>
-              <CardContent>
-                <StyledTypography variant="body2" color="text.secondary">
-                  {JSON.stringify(allStepPlayWrightQueryResult, null, 4)}
-                </StyledTypography>
-              </CardContent>
-            </Card>
           </Box>
         </Container>
       </>
